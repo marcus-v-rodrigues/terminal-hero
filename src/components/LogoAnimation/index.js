2badd9dc3442e511ad769d244d24baa1466a17cf
Marcus Rodrigues <marcus.v.rodrigues@outlook.com>
2021-04-10 19:08:05 -0300

    lottie-web implementado

--- src/components/LogoAnimation/index.js
+++ src/components/LogoAnimation/index.js
@@ -1,1 +1,25 @@
- 
+import React, { useEffect, useRef } from 'react'
+import lottie from 'lottie-web'
+
+import * as S from './styled'
+
+const LogoAnimation = () => {
+
+    const animation = useRef(null)
+
+    useEffect(() => {
+        lottie.loadAnimation({
+        container: animation.current,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        animationData: require('./animation.json')
+        })
+    }, [])
+
+    return(
+        <S.Animation ref={animation}></S.Animation>
+    )
+}
+
+export default LogoAnimation
